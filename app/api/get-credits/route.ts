9cf1d04465f933f10e9557b7433f4f75ed886c5b
sazeai <the6thsense137@gmail.com>
2024-11-03 08:57:07 +0530

    Update route.ts

--- app/api/get-credits/route.ts
+++ app/api/get-credits/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js'
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../auth/[...nextauth]/route"
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions"
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
 
