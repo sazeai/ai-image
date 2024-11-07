56d0170a0f84ef84798718d12a6366f98ed69e98
sazeai <the6thsense137@gmail.com>
2024-11-07 18:10:47 +0530

    Update route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { fal } from "@fal-ai/client"
 import { createClient } from '@supabase/supabase-js'
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../auth/[...nextauth]/route"
+import { authOptions } from "../auth/[...nextauth]/authOptions";
 
 fal.config({
   credentials: process.env.FAL_KEY
