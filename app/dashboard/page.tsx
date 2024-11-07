4452b70aeb28836c9c93c7c8416e8a31bef18fca
sazeai <the6thsense137@gmail.com>
2024-11-07 18:47:09 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Suspense } from 'react'
-import DashboardContent from './DashboardContent'
+import DashboardContent from '@/components/DashboardContent'
 
 export default function DashboardPage() {
   return (
