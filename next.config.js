0bdf8435f6c1519b117fb85aae05ceee7c916781
sazeai <the6thsense137@gmail.com>
2024-11-07 19:20:51 +0530

    Update and rename next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['replicate.delivery', 'fal.media'],
   },
@@ -9,4 +8,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
